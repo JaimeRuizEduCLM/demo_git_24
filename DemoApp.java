eed795ed1012a7cfe9fb7cb2afb7de2601c24484
JaimeRuizEduCLM <jaimeruiz_pupita@hotmail.com>
2025-05-14 21:56:02 +0200

    Initial commit

---
+++ DemoApp.java
@@ -0,0 +1,18 @@
+package es.demo_git_24;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		System.out.println("Proyecto Java para Demo");
+
+		Object cosas[]= new Object[3];
+		cosas[0]=new String("Spring");
+		cosas[1]=7;
+		cosas[2]=10.005;
+
+		for (Object obj: cosas) {
+			System.out.println(obj);
+		}
+	}
+
+}
